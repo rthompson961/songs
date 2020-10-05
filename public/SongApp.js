627ef855bdbb04a7d217644100e0b6e3496eb79f
richard <you@example.com>
2020-10-05 12:11:19 +0100

    extended song object prototype

--- public/SongApp.js
+++ public/SongApp.js
@@ -12,64 +12,68 @@
         this.$table = $table;
     };
 
-    Vote.prototype.initialize = function() {
-        this.$table.find('.js-vote-delete').on(
-            'click',
-            this.delete.bind(this)
-        );
-    };
+    $.extend(Vote.prototype, {
+        initialize: function() {
+            this.$table.find('.js-vote-delete').on(
+                'click',
+                this.delete.bind(this)
+            );
+        },
 
-    Vote.prototype.delete = function(e) {
-        e.preventDefault();
+        delete: function(e) {
+            e.preventDefault();
 
-        // change icon to red spinner
-        var $link = $(e.currentTarget);
-        $link.addClass('text-danger');
-        $link.find('span')
-            .removeClass('fa-trash-alt')
-            .addClass('fa-spinner')
-            .addClass('fa-spin');
+            // change icon to red spinner
+            var $link = $(e.currentTarget);
+            $link.addClass('text-danger');
+            $link.find('span')
+                .removeClass('fa-trash-alt')
+                .addClass('fa-spinner')
+                .addClass('fa-spin');
 
-        $.ajax({
-            url: $link.data('url'),
-            context: this,
-            method: 'DELETE',
-            success: function() {
-                $link.closest('tr').fadeOut();
-                this.updateCount($link.data('song'), $link.data('quantity'));
-                this.sortCount();
+            $.ajax({
+                url: $link.data('url'),
+                context: this,
+                method: 'DELETE',
+                success: function() {
+                    $link.closest('tr').fadeOut();
+                    this.updateCount($link.data('song'), $link.data('quantity'));
+                    this.sortCount();
+                }
+            });
+        },
+
+        updateCount: function(id, quantity) {
+            // count table row that contains the deleted vote
+            var $row = $('.js-count-row-' + id);
+            // cell that contains the count value
+            var $cell = $row.children().last();
+            // vote count minus the deleted votes
+            var newCount = $cell.html() - quantity;
+
+            // remove count row if zero votes or update the cell
+            if (newCount == 0) {
+                $row.fadeOut();
+            } else {
+                $cell.html(newCount);
             }
-        });
-    };
+        },
 
-    Vote.prototype.updateCount = function(id, quantity) {
-        // count table row that contains the deleted vote
-        var $row = $('.js-count-row-' + id);
-        // cell that contains the count value
-        var $cell = $row.children().last();
-        // vote count minus the deleted votes
-        var newCount = $cell.html() - quantity;
+        sortCount: function() {
+            var $table = $('.js-count-table tbody');
+            var $rows = $table.find('tr');
 
-        // remove count row if zero votes or update the cell
-        if (newCount == 0) {
-            $row.fadeOut();
-        } else {
-            $cell.html(newCount);
+            // sort rows by count total in descending order
+            $rows.sort(function(a, b) {
+                return $(b).children().last().html() - $(a).children().last().html();
+            });
+
+            // update count table rows to updated order
+            $.each($rows, function(index, $row) {
+                $table.append($row);
+            });
         }
-    };
+    });
 
-    Vote.prototype.sortCount = function() {
-        var $table = $('.js-count-table tbody');
-        var $rows = $table.find('tr');
 
-        // sort rows by count total in descending order
-        $rows.sort(function(a, b) {
-            return $(b).children().last().html() - $(a).children().last().html();
-        });
-
-        // update count table rows to updated order
-        $.each($rows, function(index, $row) {
-            $table.append($row);
-        });
-    };
 })(window, jQuery);
