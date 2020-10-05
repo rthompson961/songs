6eaa3fd1ee2b78bdaff6ba8c7a15880a4ed48259
richard <you@example.com>
2020-10-05 11:34:24 +0100

    replaced window and jquery global vars with arguments

--- public/SongApp.js
+++ public/SongApp.js
@@ -1,62 +1,66 @@
-var SongApp = {
-    initialize: function($table) {
-        $table.find('.js-vote-delete').on(
-            'click',
-            this.deleteVote.bind(this)
-        );
-    },
+(function(window, $) {
+    'use strict';
 
-    deleteVote: function(e) {
-        e.preventDefault();
+    window.SongApp = {
+        initialize: function($table) {
+            $table.find('.js-vote-delete').on(
+                'click',
+                this.deleteVote.bind(this)
+            );
+        },
 
-        // change icon to red spinner
-        var $link = $(e.currentTarget);
-        $link.addClass('text-danger');
-        $link.find('span')
-            .removeClass('fa-trash-alt')
-            .addClass('fa-spinner')
-            .addClass('fa-spin');
+        deleteVote: function(e) {
+            e.preventDefault();
 
-        $.ajax({
-            url: $link.data('url'),
-            context: this,
-            method: 'DELETE',
-            success: function() {
-                $link.closest('tr').fadeOut();
-                this.updateCount($link.data('song'), $link.data('quantity'));
-                this.sortCount();
+            // change icon to red spinner
+            var $link = $(e.currentTarget);
+            $link.addClass('text-danger');
+            $link.find('span')
+                .removeClass('fa-trash-alt')
+                .addClass('fa-spinner')
+                .addClass('fa-spin');
+
+            $.ajax({
+                url: $link.data('url'),
+                context: this,
+                method: 'DELETE',
+                success: function() {
+                    $link.closest('tr').fadeOut();
+                    this.updateCount($link.data('song'), $link.data('quantity'));
+                    this.sortCount();
+                }
+            });
+        },
+
+        updateCount: function(id, quantity) {
+            // count table row that contains the deleted vote
+            var $row = $('.js-count-row-' + id);
+            // cell that contains the count value
+            var $cell = $row.children().last();
+            // vote count minus the deleted votes
+            var newCount = $cell.html() - quantity;
+
+            // remove count row if zero votes or update the cell
+            if (newCount == 0) {
+                $row.fadeOut();
+            } else {
+                $cell.html(newCount);
             }
-        });
-    },
+        },
 
-    updateCount: function(id, quantity) {
-        // count table row that contains the deleted vote
-        var $row = $('.js-count-row-' + id);
-        // cell that contains the count value
-        var $cell = $row.children().last();
-        // vote count minus the deleted votes
-        var newCount = $cell.html() - quantity;
+        sortCount: function() {
+            var $table = $('.js-count-table tbody');
+            var $rows = $table.find('tr');
 
-        // remove count row if zero votes or update the cell
-        if (newCount == 0) {
-            $row.fadeOut();
-        } else {
-            $cell.html(newCount);
+            // sort rows by count total in descending order
+            $rows.sort(function(a, b) {
+                return $(b).children().last().html() - $(a).children().last().html();
+            });
+
+            // update count table rows to updated order
+            $.each($rows, function(index, $row) {
+                $table.append($row);
+            });
         }
-    },
-
-    sortCount: function() {
-        var $table = $('.js-count-table tbody');
-        var $rows = $table.find('tr');
-
-        // sort rows by count total in descending order
-        $rows.sort(function(a, b) {
-            return $(b).children().last().html() - $(a).children().last().html();
-        });
-
-        // update count table rows to updated order
-        $.each($rows, function(index, $row) {
-            $table.append($row);
-        });
     }
-}
+})(window, jQuery);
