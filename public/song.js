ec17faef617d9ad5ce8d06ad2c0d056fe8c06ac7
richard <you@example.com>
2020-10-07 21:54:27 +0100

    added fontawesome webpack css

--- public/song.js
+++ public/song.js
@@ -1,5 +1,7 @@
 const $ = require('jquery');
 require('bootstrap/dist/css/bootstrap.css');
+require('@fortawesome/fontawesome-free/css/all.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 require('./style.css');
 const SongApp = require('./SongApp');
 
