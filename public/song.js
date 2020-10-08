ae1191e63c42ef2c334953f04bc98e9e2635005a
richard <you@example.com>
2020-10-08 20:51:19 +0100

    fixed issue with spinner icon not showing

--- public/song.js
+++ public/song.js
@@ -1,7 +1,6 @@
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.css'
 import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
 import './style.css'
 import SongApp from './SongApp';
 
